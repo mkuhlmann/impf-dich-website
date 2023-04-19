e24e14ee4c0654ed05c346945424816827bd0eff
Manuel Kuhlmann <manuel@mkuhlmann.org>
2023-04-20 01:08:19 +0200

    Add mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
-
 import { astroImageTools } from 'astro-imagetools';
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind(), astroImageTools],
-	experimental: {
-		assets: true,
-	},
+  integrations: [tailwind(), astroImageTools, mdx()],
+  experimental: {
+    assets: true
+  }
 });
